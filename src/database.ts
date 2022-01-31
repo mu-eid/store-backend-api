61cd360b2099c46974b380cda4c74d23e6e3fc94
Muhammad Eid <mu.cs.eid@gmail.com>
2022-01-31 20:12:14 +0200

    Set up and configure database connection

---
+++ src/database.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+import * as pg from 'pg';
+
+import { ENV_PATH } from './utils';
+
+// SIDE EFFECT: Loading env variables into process.env
+dotenv.config({ path: ENV_PATH });
+
+const { NODE_ENV, PG_HOST, PG_USER, PG_PASS, DB_DEV, DB_TEST } = process.env;
+
+const dbClient = new pg.Pool({
+    user: PG_USER,
+    password: PG_PASS,
+    host: PG_HOST,
+    database: NODE_ENV === 'dev' ? DB_DEV : DB_TEST,
+});
+
+export default dbClient;
