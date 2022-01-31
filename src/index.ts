a88e081b8a9b3bf136dffff58e5886df3422ddf2
Muhammad Eid <mu.cs.eid@gmail.com>
2022-01-31 20:28:37 +0200

    Set up server

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+
+import { ENV_PATH } from './utils';
+import dbClient from './database';
+
+const ENV_VARS = dotenv.config({ path: ENV_PATH });
+
+const { APP_HOST, APP_PORT, NODE_ENV } = process.env;
+
+const app = express();
+
+app.get('/hallo', async (req: Request, resp: Response): Promise<void> => {
+    try {
+        const conn = await dbClient.connect();
+        const sqlQuery = `SELECT $1 AS greeting;`;
+        const result = await conn.query(sqlQuery, ['Leute!']);
+        conn.release();
+        resp.json({ hallo: result.rows[0]['greeting'] });
+    } catch (err) {
+        resp.status(500).json({
+            error: {
+                When: 'While requesting GET /hallo',
+                Reason: (err as Error).message,
+            },
+        });
+    }
+});
+
+app.listen(APP_PORT, (): void => {
+    console.log(`Server is listening at http://${APP_HOST}:${APP_PORT}`);
+
+    if (ENV_VARS.error) console.log(ENV_VARS.error);
+    else if (NODE_ENV === 'dev') console.log(ENV_VARS.parsed);
+});
