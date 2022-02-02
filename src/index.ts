146c82cfc86ddae639e66d9649b99b48e80b2731
Muhammad Eid <mu.cs.eid@gmail.com>
2022-02-02 15:30:00 +0200

    Adjust the path to utils/path module

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
-
-import { ENV_PATH } from './utils';
+import * as bcrypt from 'bcrypt';
+import { ENV_PATH } from './utils/path';
 import dbClient from './database';
+import { init_db } from './utils/db_migrator';
 
 const ENV_VARS = dotenv.config({ path: ENV_PATH });
-
 const { APP_HOST, APP_PORT, NODE_ENV } = process.env;
+
+init_db();
 
 const app = express();
 
