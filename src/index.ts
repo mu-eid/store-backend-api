45884ad5f60dc79d5752b04c777627e8305d6722
Muhammad Eid <mu.cs.eid@gmail.com>
2022-02-11 20:53:42 +0200

    Remove debugging code from index file

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,34 @@
-import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
-// import * as bcrypt from 'bcrypt';
 import { ENV_PATH } from './utils/path';
-import dbClient from './database';
+dotenv.config({ path: ENV_PATH });
+
+import express, { Request, Response } from 'express';
 import { initDevDB } from './utils/db_migrator';
 import userRoutes from './handlers/user.routes';
 import productRoutes from './handlers/product.routes';
 import orderRoutes from './handlers/order.routes';
 import itemRoutes from './handlers/order_item.routes';
 
-const ENV_VARS = dotenv.config({ path: ENV_PATH });
-const { HOST, PORT } = process.env;
-
-process.on('uncaughtException', (err) => console.error(err));
-
-// Start clean DB migrations
+// Start DB migrations
 initDevDB();
 
 export const app = express();
 
-// Use JSON body parser and attach API endpoints
+// Use JSON body parser
 app.use(express.json());
+
+// Attach API endpoints
 userRoutes(app);
 productRoutes(app);
 orderRoutes(app);
 itemRoutes(app);
 
-app.get('/hallo', async (req: Request, resp: Response): Promise<void> => {
-    try {
-        const conn = await dbClient.connect();
-        const sqlQuery = `SELECT $1 AS greeting;`;
-        const result = await conn.query(sqlQuery, ['Leute!']);
-        conn.release();
-        resp.json({ hallo: result.rows[0]['greeting'] });
-    } catch (err) {
-        resp.status(500).json({
-            error: {
-                When: 'While requesting GET /hallo',
-                Reason: (err as Error).message,
-            },
-        });
-    }
+app.get('/status', async (req: Request, resp: Response): Promise<void> => {
+    resp.json({ api: { service: 'up' } });
 });
+
+const { HOST, PORT } = process.env;
 
 app.listen(PORT, async (): Promise<void> => {
     console.log(`Server is listening at http://${HOST}:${PORT}`);
-
-    if (ENV_VARS.error) console.log(ENV_VARS.error);
-
-    console.log(ENV_VARS.parsed);
 });
