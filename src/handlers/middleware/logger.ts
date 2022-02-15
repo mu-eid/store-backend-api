f670d3c040c66d6949ba4187dde53c0b5d4f1a43
Muhammad Eid <mu.cs.eid@gmail.com>
2022-02-15 02:35:56 +0200

    Modify logger middleware to stop in test mode

--- src/handlers/middleware/logger.ts
+++ src/handlers/middleware/logger.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 
-function logger(req: Request, resp: Response, next: NextFunction): void {
-    const date = new Date().toISOString();
-    console.log(`[LOG]: ${req.method} ${req.path} -- TIME: ${date}`);
+const logger = (req: Request, resp: Response, next: NextFunction): void => {
+    if (process.env.NODE_ENV === 'dev') logRequest(req);
     next();
-}
+};
+
+const logRequest = (req: Request): void => {
+    const date = new Date().toUTCString();
+    console.log(`[LOG]: ${req.method} ${req.path} -- TIMESTAMP: ${date}`);
+};
 
 export { logger };
