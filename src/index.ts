85307c8db47207a03c9e3bfdc5b0db02f48a18fa
Muhammad Eid <mu.cs.eid@gmail.com>
2022-02-15 19:05:18 +0200

    Improve api starting up log messages and adding /admin endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,21 @@ import { ENV_PATH } from './utils/path';
 dotenv.config({ path: ENV_PATH });
 
 import express, { Request, Response } from 'express';
+
 import { initDevDB } from './utils/db_migrator';
 import userRoutes from './handlers/user.routes';
 import productRoutes from './handlers/product.routes';
 import orderRoutes from './handlers/order.routes';
 import itemRoutes from './handlers/order_item.routes';
 import { logger } from './handlers/middleware/logger';
+import { getAdminToken } from './utils/user';
 
-// Start DB migrations
-initDevDB();
+// Start database migrations
+process.nextTick(async () => {
+    if (process.env.NODE_ENV === 'dev') {
+        await initDevDB();
+    }
+});
 
 export const app = express();
 
@@ -26,11 +32,18 @@ orderRoutes(app);
 itemRoutes(app);
 
 app.get('/status', async (req: Request, resp: Response): Promise<void> => {
-    resp.json({ api: { service: 'up' } });
+    resp.json({ api: { status: 'up' } });
+});
+
+app.get('/admin', (req: Request, resp: Response): void => {
+    resp.json({
+        message: 'Admin user token',
+        token: getAdminToken(),
+    });
 });
 
 const { HOST, PORT } = process.env;
 
-app.listen(PORT, async (): Promise<void> => {
-    console.log(`Server is listening at http://${HOST}:${PORT}`);
+app.listen(PORT, ():void => {
+    console.log(`Server is listening at http://${HOST}:${PORT}/status`);
 });
