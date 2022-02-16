575cc07019548e60f4c1e892c1c9ee128ae62890
Muhammad Eid <mu.cs.eid@gmail.com>
2022-02-16 15:18:12 +0200

    Modify database configuration variables

--- src/database.ts
+++ src/database.ts
@@ -6,12 +6,12 @@ import { ENV_PATH } from './utils/path';
 // SIDE EFFECT: Loading env variables into process.env
 dotenv.config({ path: ENV_PATH });
 
-const { PG_HOST, PG_USER, PG_PASS, DB_DEV, DB_TEST } = process.env;
+const { HOST, PG_USER, PG_PASS, DB_DEV, DB_TEST } = process.env;
 
 const dbClient = new pg.Pool({
     user: PG_USER,
     password: PG_PASS,
-    host: PG_HOST,
+    host: HOST,
     database: process.env.NODE_ENV === 'dev' ? DB_DEV : DB_TEST,
 });
 
